8b175e7e775da097ce0220d14ebfc153175a90cc
Raquel <raque@DESKTOP-FOIJBSU.(none)>
2020-03-16 13:18:58 -0400

    update

---
+++ CardDemo/app/src/main/java/com/ebookfrenzy/carddemo/Data.java
@@ -0,0 +1,35 @@
+package com.ebookfrenzy.carddemo;
+
+public class Data {
+    private String[] titles = {"Chapter One",
+            "Chapter Two",
+            "Chapter Three",
+            "Chapter Four",
+            "Chapter Five",
+            "Chapter Six",
+            "Chapter Seven",
+            "Chapter Eight"};
+    private String[] details = {"Item one details",
+            "Item two details", "Item three details",
+            "Item four details", "Item five details",
+            "Item six details", "Item seven details",
+            "Item eight details"};
+    private int[] images = { R.drawable.android_image_1,
+            R.drawable.android_image_2,
+            R.drawable.android_image_3,
+            R.drawable.android_image_4,
+            R.drawable.android_image_5,
+            R.drawable.android_image_6,
+            R.drawable.android_image_7,
+            R.drawable.android_image_8 };
+
+    public String[] getTitles(){
+        return titles;
+    }
+    public String[] getDetails(){
+        return details;
+    }
+    public int[] getimages(){
+        return images;
+    }
+}
---
+++ CardDemo/app/src/main/java/com/ebookfrenzy/carddemo/RecyclerAdapter.java
@@ -0,0 +1,67 @@
+package com.ebookfrenzy.carddemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Random;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.
+        ViewHolder> {
+    Data data = new Data();
+    private final int max = 7;
+    private final int min = 0;
+
+
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView itemImage;
+        TextView itemTitle;
+        TextView itemDetail;
+
+        ViewHolder(View itemView){
+            super(itemView);
+            itemImage = itemView.findViewById(R.id.item_image);
+            itemTitle = itemView.findViewById(R.id.item_title);
+            itemDetail = itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item"+position,
+                    Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int random = new Random().nextInt((max-min)-1)-min;
+        holder.itemTitle.setText(data.getTitles()[random]);
+        holder.itemDetail.setText(data.getDetails()[random]);
+        holder.itemImage.setImageResource(data.getimages()[random]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.getTitles().length;
+    }
+}
